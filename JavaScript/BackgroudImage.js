b7afb4fa7e1a3b30a97e847f20e713efa667be55
Wuzhi <3550522631xy@gmail>
2024-09-02 21:10:48 +0800

    111

--- JavaScript/BackgroudImage.js
+++
@@ -1,6 +0,0 @@
-// 在页面加载完成后，添加 loaded 类
-document.addEventListener("DOMContentLoaded", function () {
-    document.body.classList.add("loaded");
-    // 修改 CSS 变量的值以更新背景图
-    document.documentElement.style.setProperty('--background-image-url', "url('./PIC/01.jpg')");
-});
