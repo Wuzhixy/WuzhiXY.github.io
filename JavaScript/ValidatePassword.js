b6cd22b595bb6cefda350ac52ae13444ef3dbc2e
Wuzhi <3550522631xy@gmail>
2024-09-03 20:13:24 +0800

    111

--- JavaScript/ValidatePassword.js
+++ JavaScript/ValidatePassword.js
@@ -1,8 +1,8 @@
 function validatePassword() {
-    var password = prompt("请输入密码以进入日记本:"); // 提示用户输入密码
+    var password = prompt("请输入密码以进入雾质的记忆:"); // 提示用户输入密码
 
     if (password === "040610") { // 检查输入的密码是否正确
-        window.location.href = "./Menu/Diary/Diary.html"; // 如果密码正确，跳转到日记本页面
+        window.location.href = "../../Memory.html"; // 如果密码正确，跳转到日记本页面
     } else {
         alert("密码错误，请重试！"); // 如果密码错误，显示提示信息
     }
